c63467ab733f4d6a50e6e917a2fa9bdcd3474ba7
Ryan Gonzales <helloryantg@gmail.com>
2018-11-26 21:31:34 -0800

    fix signup and login auth

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,5 +1,15 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  // Redirect
+} from 'react-router-dom';
+import userService from '../../utils/userService';
 import './App.css';
+import GamePage from '../GamePage/GamePage';
+import SignupPage from '../SignupPage/SignupPage';
+import LoginPage from '../LoginPage/LoginPage';
 
 class App extends Component {
   constructor(props) {
@@ -8,11 +18,45 @@ class App extends Component {
       // Put state in here
     }
   }
+
+  handleLogout = () => {
+    userService.logout();
+    this.setState({user: null});
+  }
+
+  handleSignupOrLogin = () => {
+    this.setState({user: userService.getUser()});
+  }
+
+  /*----- Lifecycle Methods -----*/
+
+  componentDidMount() {
+    let user = userService.getUser();
+    this.setState({user});
+  }
   
   render() {
     return (
       <div className="App">
         <header>Complete Me!</header>
+        <Router>
+          <Switch>
+            <Route exact path='/' render={() =>
+              <GamePage /> 
+            } />
+            <Route exact path ='/signup' render={({history}) => 
+              <SignupPage 
+                history={history}
+              />
+            } />
+            <Route exact path='/login' render={(props) =>
+              <LoginPage 
+                {...props}
+                handleSignupOrLogin={this.handleSignupOrLogin}
+              />
+            }/>
+          </Switch>
+        </Router>
       </div>
     );
   }
